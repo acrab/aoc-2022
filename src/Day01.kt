4496efa7944cb572748baf79167b52a83792212e
acrab <acrab@live.co.uk>
2022-12-01 22:53:16 +0000

    Complete Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
+import com.google.common.truth.Truth.assertThat
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var current = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                if (current > max) {
+                    max = current
+                }
+                current = 0
+            } else {
+                current += line.toInt()
+            }
+        }
+        if (current > max) {
+            max = current
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val groups = mutableListOf<Int>()
+        var current = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                groups.add(current)
+                current = 0
+            } else {
+                current += line.toInt()
+            }
+        }
+        groups.add(current)
+        return groups.sortedDescending().take(3).reduce { a, b -> a + b }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    assertThat(part1(testInput)).isEqualTo(24000)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
+    assertThat(part2(testInput)).isEqualTo(45000)
+
     println(part2(input))
 }
